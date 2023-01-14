049afe6d480f0a6bfa7d2edecc9450c176ddfc09
Raul Macarie <raul@macarie.me>
2023-01-14 19:28:16 +0100

    chore: add eslint and prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,4 +2,65 @@
 module.exports = {
 	root: true,
 	reportUnusedDisableDirectives: true,
-}
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: ["tsconfig.cjs.json", "tsconfig.vite.json", "tsconfig.json"],
+	},
+	plugins: ["prettier", "solid"],
+	extends: [
+		"xo",
+		"xo-typescript",
+		"plugin:import/recommended",
+		"plugin:unicorn/recommended",
+		"plugin:jsx-a11y/recommended",
+		"plugin:solid/typescript",
+		"plugin:prettier/recommended",
+	],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				useTabs: true,
+				trailingComma: "all",
+			},
+		],
+		"unicorn/prefer-at": [
+			"error",
+			{
+				checkAllIndexAccess: true,
+			},
+		],
+		"@typescript-eslint/consistent-type-definitions": ["error", "type"],
+		"@typescript-eslint/consistent-type-imports": [
+			"error",
+			{
+				prefer: "type-imports",
+			},
+		],
+		"@typescript-eslint/naming-convention": "off",
+		"import/no-unresolved": "off",
+		"import/order": [
+			"error",
+			{
+				alphabetize: {
+					caseInsensitive: true,
+					order: "asc",
+				},
+				groups: [
+					[
+						"builtin",
+						"external",
+						"internal",
+						"unknown",
+						"parent",
+						"sibling",
+						"index",
+						"object",
+					],
+					"type",
+				],
+				"newlines-between": "always",
+			},
+		],
+	},
+};
